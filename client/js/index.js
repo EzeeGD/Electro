e29c2f5c4e34252cd118d585dddaa45cd7a04c04
Ezequiel Guardia <ezequiel.guardia413@gmail.com>
2025-06-13 11:14:51 -0300

    Actualizacion 1.0
    Se crea la pagina, aun falta completar mas productos y mejoras en la interfaz

---
+++ client/js/index.js
@@ -0,0 +1,52 @@
+const cardContainer = document.getElementById("cardContainer")
+const URL_Productos = './data/productos.json'
+const productos = []
+
+function retornarCardHTML(producto) {
+    return `
+        <div class="card">
+            <div class="card-name">${producto.nombre}</div>
+            <img src="${producto.imagen}" alt="${producto.nombre}">
+            <div class="card-description">${producto.description}</div>
+            <div class="card-price">$${producto.precio}</div>
+        </div>`
+}
+
+function promptErrorCard() {
+    const errorCard = document.getElementById('errorCard');
+    if (errorCard === null) {
+        console.error("No se pudo obtener el elemento #errorCard")
+        return
+    }
+    if (productos.length == 0) {
+        errorCard.className = "card-error ocultar";
+    } else {
+        errorCard.className = "card-error";
+    }
+}
+
+function cargarProductos(array) {
+    console.table(array);
+    if (cardContainer == null) {
+        console.error("Cannot find container for cards.")
+        return
+    }
+    array.forEach((element) => {
+        cardContainer.innerHTML += retornarCardHTML(element)
+    });
+    activarClickEnBotones()
+}
+
+const obtenerProductos = () => {
+    fetch(URL_Productos)
+        .then((response) => response.json())
+        .then((data) => productos.push(...data))
+        .then(() => cargarProductos(productos))
+        .catch((error) => console.error(error))
+    if (productos.length != 0) {
+        cargarProductos(productos);
+    }
+    promptErrorCard()
+}
+
+obtenerProductos()
